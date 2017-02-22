dda009b0b3e969e7650f417d7bda0a55dfebdd7d
Javier <javier.mac13@gmail.com>
2017-02-22 18:07:43 +0000

    step 9 complete

--- app/views.py
+++ app/views.py
@@ -28,26 +28,24 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.validate_on_submit():
-            # Get the username and password values from the form.
+    if request.method == 'POST' and form.validate_on_submit():
+        
+        #if form.username.data:
             username = form.username.data
             password = form.password.data
-            
-            # using your model, query database for a user based on the username
-            # and password submitted
-            # store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method.
             user = UserProfile.query.filter_by(username=username, password=password).first()
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            flash('Logged in Successfully.', 'success')
-            return redirect(url_for("secure-page")) # they should be redirected to a secure-page route instead
+            if user is not None:
+            
+                login_user(user)
+                flash('Logged in Successfully.', 'success')
+                next = request.args.get('next')
+            
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+                return redirect(url_for('login'))
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
@@ -55,6 +53,11 @@ def login():
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
+    
+@app.route('/secure-page/')
+@login_required
+def secure_page():
+    return render_template('secure-page.html')
 
 ###
 # The functions below should be applicable to all Flask apps.
